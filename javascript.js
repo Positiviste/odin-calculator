8af51edec7a4864932d1f66f7b63b17af258e1be
Positiviste <106838114+Positiviste@users.noreply.github.com>
2023-01-24 22:06:17 +0100

    Operators and clear buttons done

--- javascript.js
+++ javascript.js
@@ -1,32 +1,97 @@
-let result1 = 3;
-let result2 = 6;
+let result1 = "0";
+let result2 = "0";
+let activeOperator = "plus";
+
+const activeNumber = document.querySelector("#activeNumber");
+const memorisedNumber = document.querySelector("#memorisedNumber");
+const clearAll = document.querySelector("#clearAll");
+const clear = document.querySelector("#clear");
 
 function add(a, b) {
-    return (a + b);
+    return ((+a) + (+b));
 }
 
 function subtract(a, b) {
-    return (a - b);
+    return ((+a) - (+b));
 }
 
 function multiply(a, b) {
-    return (a * b);
+    return ((+a) * (+b));
 }
 
 function divide(a, b) {
-    return (a / b);
+    return ((+a) / (+b));
 }
 
-function operate(operator, a, b) {
-    switch (operator) {
-        case "+" : add(a, b);
-        case "-" : subtract(a, b);
-        case "*" : multiply(a, b);
-        case "/" : divide(a, b);
+function operate() {
+    console.log(result1 + activeOperator + result2);
+    switch (activeOperator) {
+        case "plus": result1 = add(result1, result2);
+            result2 = 0;
+            break;
+        case "subtract": result1 = subtract(result1, result2);
+            result2 = 0;
+            break;
+        case "multiply": result1 = multiply(result1, result2);
+            result2 = 0;
+            break;
+        case "divide": result1 = divide(result1, result2);
+            result2 = 0;
+            break;
+        // case "equal": result1 = result2
+        //     result2 = 0;
+        //     break;
+    };
+    memorisedNumber.textContent = result1;
+}
+
+function listenNumber() {
+    const numberButton = document.querySelectorAll('.number');
+    Array.from(numberButton).forEach(number => {
+        number.addEventListener('click', function (number) {
+            result2 += this.id;
+            refreshActiveNumber()
+        });
+    });
+}
+
+function refreshActiveNumber() {
+    if (!result2.includes(".")) {
+        result2 = Math.round(result2);
     }
+    activeNumber.textContent = result2;
 }
 
-console.log (result1+result2);
-console.log (result1-result2);
-console.log (result1*result2);
-console.log (result1/result2);
+function listenOperator() {
+    const operatorButton = document.querySelectorAll('.operator');
+    Array.from(operatorButton).forEach(operator => {
+        operator.addEventListener('click', function (operator) {
+            operate();
+            if (this.id != "equal") {
+                activeOperator = this.id;
+            }
+        });
+    });
+}
+
+function listenClear() {
+    const clearButton = document.querySelectorAll('.clear');
+    Array.from(clearButton).forEach(clear => {
+        clear.addEventListener('click', function (clear) {
+            result2 = "0";
+            activeNumber.textContent = result2;
+            console.log("clear");
+            if (this.id == "clearAll") {
+                result1 = "0";
+                memorisedNumber.textContent = result1;
+                activeOperator = "plus";
+                console.log("clearAll");
+            }
+        });
+    });
+}
+
+
+listenNumber();
+listenOperator();
+listenClear();
